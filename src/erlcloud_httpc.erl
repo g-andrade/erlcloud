4f05e73518fafddf2e4a7878e3f29b2cfa3b5a98
Guilherme Andrade <g@gandrade.net>
2014-11-26 11:42:52 +0000

    lhttpc, multiple pools - the dirty way.

--- src/erlcloud_httpc.erl
+++ src/erlcloud_httpc.erl
@@ -11,5 +11,21 @@
 
 -export([request/6]).
 
+
+-define(LHTTPC_POOLID_PADDING, 2).
+
 request(URL, Method, Hdrs, Body, Timeout, _Config) ->
-    lhttpc:request(URL, Method, Hdrs, Body, Timeout, []).
+    Pools = application:get_env(erlcloud, lhttpc_pools, 1),
+    PoolIndex = crypto:rand_uniform(0, Pools),
+    PoolPreffix = application:get_env(erlcloud, lhttpc_pool_preffix, "lhttpc_man_erlcloud"),
+
+    PoolSuffix = case PoolIndex >= (?LHTTPC_POOLID_PADDING * 10) of
+        true  -> integer_to_list(PoolIndex);
+        false ->
+            FormatStr = lists:flatten(["~", integer_to_list(?LHTTPC_POOLID_PADDING), "..0B"]),
+            lists:flatten( io_lib:format(FormatStr, [PoolIndex]) )
+    end,
+
+    PoolId = list_to_atom(PoolPreffix ++ PoolSuffix),
+    LHttpcConfig = [{pool, PoolId}, {pool_ensure, true}],
+    lhttpc:request(URL, Method, Hdrs, Body, Timeout, LHttpcConfig).
